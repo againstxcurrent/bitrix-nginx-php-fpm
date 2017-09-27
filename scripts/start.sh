0bddde85722aa2e65e76a87480f474e0467c21c9
againstxcurrent <30798962+againstxcurrent@users.noreply.github.com>
2017-09-27 11:55:12 +0300

    1
    1

---
+++ scripts/start.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+# Disable Strict Host checking for non interactive git clones
+
+mkdir -p -m 0700 /root/.ssh
+# Prevent config files from being filled to infinity by force of stop and restart the container 
+echo "" > /root/.ssh/config
+echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
+
+if [[ "$GIT_USE_SSH" == "1" ]] ; then
+  echo -e "Host *\n\tUser ${GIT_USERNAME}\n\n" >> /root/.ssh/config
+fi
+
+if [ ! -z "$SSH_KEY" ]; then
+ echo $SSH_KEY > /root/.ssh/id_rsa.base64
+ base64 -d /root/.ssh/id_rsa.base64 > /root/.ssh/id_rsa
+ chmod 600 /root/.ssh/id_rsa
+fi
+
+# Set custom webroot
+if [ ! -z "$WEBROOT" ]; then
+ sed -i "s#root /var/www/html;#root ${WEBROOT};#g" /etc/nginx/sites-available/default.conf
+else
+ webroot=/var/www/html
+fi
+
+# Enable custom nginx config files if they exist
+if [ -f /var/www/html/conf/nginx/nginx.conf ]; then
+  cp /var/www/html/conf/nginx/nginx.conf /etc/nginx/nginx.conf
+fi
+
+if [ -f /var/www/html/conf/nginx/nginx-site.conf ]; then
+  cp /var/www/html/conf/nginx/nginx-site.conf /etc/nginx/sites-available/default.conf
+fi
+
+if [ -f /var/www/html/conf/nginx/nginx-site-ssl.conf ]; then
+  cp /var/www/html/conf/nginx/nginx-site-ssl.conf /etc/nginx/sites-available/default-ssl.conf
+fi
+
+
+# Prevent config files from being filled to infinity by force of stop and restart the container
+lastlinephpconf="$(grep "." /usr/local/etc/php-fpm.conf | tail -1)"
+if [[ $lastlinephpconf == *"php_flag[display_errors]"* ]]; then
+ sed -i '$ d' /usr/local/etc/php-fpm.conf
+fi
+
+# Display PHP error's or not
+if [[ "$ERRORS" != "1" ]] ; then
+ echo php_flag[display_errors] = off >> /usr/local/etc/php-fpm.conf
+else
+ echo php_flag[display_errors] = on >> /usr/local/etc/php-fpm.conf
+fi
+
+# Display Version Details or not
+if [[ "$HIDE_NGINX_HEADERS" == "0" ]] ; then
+ sed -i "s/server_tokens off;/server_tokens on;/g" /etc/nginx/nginx.conf
+else
+ sed -i "s/expose_php = On/expose_php = Off/g" /usr/local/etc/php-fpm.conf
+fi
+
+# Increase the memory_limit
+if [ ! -z "$PHP_MEM_LIMIT" ]; then
+ sed -i "s/memory_limit = 128M/memory_limit = ${PHP_MEM_LIMIT}M/g" /usr/local/etc/php/conf.d/docker-vars.ini
+fi
+
+# Increase the post_max_size
+if [ ! -z "$PHP_POST_MAX_SIZE" ]; then
+ sed -i "s/post_max_size = 100M/post_max_size = ${PHP_POST_MAX_SIZE}M/g" /usr/local/etc/php/conf.d/docker-vars.ini
+fi
+
+# Increase the upload_max_filesize
+if [ ! -z "$PHP_UPLOAD_MAX_FILESIZE" ]; then
+ sed -i "s/upload_max_filesize = 100M/upload_max_filesize= ${PHP_UPLOAD_MAX_FILESIZE}M/g" /usr/local/etc/php/conf.d/docker-vars.ini
+fi
+
+# Enable xdebug
+XdebugFile='/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini'
+if [[ "$ENABLE_XDEBUG" == "1" ]] ; then
+  if [ -f $XdebugFile ]; then
+  	echo "Xdebug enabled"
+  else
+  	echo "Enabling xdebug"
+  	echo "If you get this error, you can safely ignore it: /usr/local/bin/docker-php-ext-enable: line 83: nm: not found"
+  	# see https://github.com/docker-library/php/pull/420
+    docker-php-ext-enable xdebug
+    # see if file exists
+    if [ -f $XdebugFile ]; then
+        # See if file contains xdebug text.
+        if grep -q xdebug.remote_enable "$XdebugFile"; then
+            echo "Xdebug already enabled... skipping"
+        else
+            echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > $XdebugFile # Note, single arrow to overwrite file.
+            echo "xdebug.remote_enable=1 "  >> $XdebugFile
+            echo "xdebug.remote_log=/tmp/xdebug.log"  >> $XdebugFile
+            echo "xdebug.remote_autostart=false "  >> $XdebugFile # I use the xdebug chrome extension instead of using autostart
+            # NOTE: xdebug.remote_host is not needed here if you set an environment variable in docker-compose like so `- XDEBUG_CONFIG=remote_host=192.168.111.27`.
+            #       you also need to set an env var `- PHP_IDE_CONFIG=serverName=docker`
+        fi
+    fi
+  fi
+else
+    if [ -f $XdebugFile ]; then
+        echo "Disabling Xdebug"
+      rm $XdebugFile
+    fi
+fi
+
+if [ ! -z "$PUID" ]; then
+  if [ -z "$PGID" ]; then
+    PGID=${PUID}
+  fi
+  deluser nginx
+  addgroup -g ${PGID} nginx
+  adduser -D -S -h /var/cache/nginx -s /sbin/nologin -G nginx -u ${PUID} nginx
+else
+  if [ -z "$SKIP_CHOWN" ]; then
+    chown -Rf nginx.nginx /var/www/html
+  fi
+fi
+
+# Run custom scripts
+if [[ "$RUN_SCRIPTS" == "1" ]] ; then
+  if [ -d "/var/www/html/scripts/" ]; then
+    # make scripts executable incase they aren't
+    chmod -Rf 750 /var/www/html/scripts/*
+    # run scripts in number order
+    for i in `ls /var/www/html/scripts/`; do /var/www/html/scripts/$i ; done
+  else
+    echo "Can't find script directory"
+  fi
+fi
+
+# Try auto install for composer
+if [ -f "/var/www/html/composer.lock" ]; then
+    if [ "$APPLICATION_ENV" == "development" ]; then
+        composer global require hirak/prestissimo
+        composer install --working-dir=/var/www/html
+    else
+        composer global require hirak/prestissimo
+        composer install --no-dev --working-dir=/var/www/html
+    fi
+fi
+
+# Start supervisord and services
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
+
